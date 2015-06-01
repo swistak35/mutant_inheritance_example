f1316cab99ceaafe8aa2f0aea0a79a90236bdcba
Rafał Łasocha <me+dev@swistak35.com>
2015-06-01 15:59:23 +0200

    up

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -5,4 +5,13 @@ describe Foo do
 
     expect(f.arr).to eq([1,2,3])
   end
+
+  it "" do
+    f = Foo.new
+    y = [1,2,3]
+    f.set_something(y)
+
+    f.set_something([4,5,6])
+    expect(y).to be_empty
+  end
 end
